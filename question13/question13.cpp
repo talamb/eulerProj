aa52f166b8345c76dc84ec2f4eaba3f84a3c8dd6
Timothy Lamb <talamb.cs@gmail.com>
2016-04-01 18:00:12 -0500

    start the main for problem 13

---
+++ question13/question13.cpp
@@ -0,0 +1,34 @@
+/*------------------------------------------------------
+ * Filename: question13.cpp
+ * Developer: Drew Lamb
+ *
+ * Created On: 4/1/16
+ * Last Updated: 4/1/16
+ *
+ * Description: Prints the first 10 digits of the sum of a very large number.
+ * ---------------------------------------------------------*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	//make sure the file is supplied
+	if(argc < 2)
+	{
+		cout<< argv[0] << " requires a file for argument."<< endl;
+	}
+
+	unsigned int sum = 0;
+	ifstream fin(argv[1]);
+	string cur_num;
+
+	while(getline
+	
+	
+	
+}
